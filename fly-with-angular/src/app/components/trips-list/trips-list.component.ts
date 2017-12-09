946ef68e02952b06c5d9cc5bdfaf0c2ecb5b5a1c
aatish <aatish@vacationlabs.com>
2017-12-09 16:41:24 +0530

    button

--- fly-with-angular/src/app/components/trips-list/trips-list.component.ts
+++ fly-with-angular/src/app/components/trips-list/trips-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Trip } from '../../models/trip';
-
+import { BrowserPlatformLocation } from '@angular/platform-browser/src/browser/location/browser_platform_location';
+import { ReactiveFormsModule } from '@angular/forms';
+import {FormGroup, FormBuilder, FormControl} from '@angular/forms';
 @Component({
   selector: 'app-trips-list',
   templateUrl: './trips-list.component.html',
@@ -9,7 +11,7 @@ import { Trip } from '../../models/trip';
 export class TripsListComponent implements OnInit {
 
   title = 'Flights of a Lifetime';
-
+  flag = false;
   trips: Array<Trip> = [
     {
       name: 'Super Flights to Mars',
@@ -37,6 +39,18 @@ export class TripsListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const form = new FormGroup({
+      tripname: new FormControl(),
+      tripcost: new FormControl(),
+      tripduration: new FormControl(),
+      tripdescription: new FormControl()
+    });
+    
   }
-
+  // createForm(){
+  //   this.ftripForm - this.fb.
+  // }
+showtrip(){
+  this.flag = !this.flag;
+}
 }
